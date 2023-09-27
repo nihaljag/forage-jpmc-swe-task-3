b17ff33b07cf409911a00928a4ad291c0976b650
Nihal J <72304446+nihaljag@users.noreply.github.com>
2023-09-27 21:21:16 +0530

    fixed for const bounds for trigger

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -1,20 +1,32 @@
 import { ServerRespond } from './DataStreamer';
 
 export interface Row {
-  stock: string,
-  top_ask_price: number,
+  price_abc: number,
+  price_def: number,
+  ratio: number,
+  upper_bound: number,
+  lower_bound: number,
+  trigger_alert: number | undefined,
   timestamp: Date,
 }
 
 
 export class DataManipulator {
-  static generateRow(serverResponds: ServerRespond[]) {
-    return serverResponds.map((el: any) => {
+  static generateRow(serverRespond: ServerRespond[]): Row {
+      const priceABC = (serverRespond[0].top_ask.price + serverRespond[0].top_bid.price)/2;
+      const priceDEF = (serverRespond[1].top_ask.price + serverRespond[1].top_bid.price)/2;
+      const ratio = priceABC/priceDEF;
+      const upper_bound = 1+0.10;
+      const lower_bound = 1-0.10;
       return {
-        stock: el.stock,
-        top_ask_price: el.top_ask && el.top_ask.price || 0,
-        timestamp: el.timestamp,
+        price_abc: priceABC,
+        price_def: priceDEF,
+        ratio: ratio,
+        upper_bound: upper_bound,
+        lower_bound: lower_bound,
+        trigger_alert: (ratio>upper_bound || ratio<lower_bound)?ratio : undefined,
+        timestamp: serverRespond[0].timestamp>serverRespond[1].timestamp?serverRespond[0].timestamp:serverRespond[1].timestamp,
       };
-    })
+    
   }
 }
